5c7149fdb11a73dc51fc7210a06d4a159af3ff85
EvgeneOskin <eoskin@crystalnix.com>
2018-09-01 23:10:15 +0600

    Add CORS headers based on enable-cors.org

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -u
 
-envsubst "\${HOST_NAME} \${APP_UPSTREAM}" < /etc/nginx/app.conf > /etc/nginx/conf.d/app.conf
+envsubst "\${HOST_NAME} \${APP_UPSTREAM} \${CORS_ORIGINS}" < /etc/nginx/app.conf > /etc/nginx/conf.d/app.conf
 if [ -n "${SUPPORT_UPGRADE:-}" ]; then
 cat >> /etc/nginx/conf.d/app.conf <<EOF
 proxy_set_header Upgrade \$http_upgrade;
